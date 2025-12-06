faa6e8e499499a59fe228bbd702d1ee33a4f8b31
spiderites <spiderites@gmail.com>
2025-12-06 18:54:25 +0800

    dev fix

--- routes/dev.tsx
+++ routes/dev.tsx
@@ -1,6 +1,7 @@
 // routes/dev.tsx
 import { Head } from "fresh/runtime";
 import DevComments from "../islands/DevComments.tsx";
+import Love from "../islands/Love.tsx";
 
 export default function DevPage() {
   return (
